4cec9717301b32ba5c59d7de5097a9d81a914103
SeungminHwang <hwangtmdals@gmail.com>
2024-08-02 11:55:08 +0900

    add TODOs and remove servo motor supports

--- 2024_KSOP_handwriting_robot/2024_KSOP_handwriting_robot.ino
+++ 2024_KSOP_handwriting_robot/2024_KSOP_handwriting_robot.ino
@@ -1,26 +1,14 @@
-#include "StepperAsync5.h"
-#include <Servo.h>
 
-// contants
-const int steps_per_revolution = 2048;
-const int axis_diameter = 165; // in mm
-const int wheel_diameter = 53; // in mm
-
-
-
-// Stepper motor settings
-StepperAsync5 stepper_l(steps_per_revolution, 11, 9, 10, 8);
-StepperAsync5 stepper_r(steps_per_revolution, 5, 3, 4, 2);
 
 
 // Servo motor settings
-# define SERVO_PIN 6
-Servo pen_servo;
+//# define SERVO_PIN 6
+//Servo pen_servo;
 
 
-void forward(int distance)
+void forward(float distance)
 {
-  int steps = distance_to_steps(distance);
+  int steps; // TODO = distance_to_steps(distance);
   stepper_l.setStep(steps);
   stepper_r.setStep(steps);
 
@@ -35,7 +23,7 @@ void forward(int distance)
 
 }
 
-void backward(int distance)
+void backward(float distance)
 {
   forward(-distance);
 }
@@ -52,10 +40,8 @@ void turn_right(int cnt = 1)
 
 void rotate_by_angle(int degree)
 {
-  // TODO
-  // (pi * axis_diameter) / 4;
-  int distance = int(PI * axis_diameter / 4 * degree / 90);
-  int steps = distance_to_steps(distance);
+  float distance; // TODO
+  int steps; // TODO
 
   stepper_l.setStep(steps);
   stepper_r.setStep(-steps);
@@ -70,6 +56,7 @@ void rotate_by_angle(int degree)
 
 }
 
+/*
 void pen_up()
 {
   // TODO
@@ -81,13 +68,14 @@ void pen_down()
   // TODO
   pen_servo.write(30); // fix
 }
+*/
 
 
 // convert distance to step
-int distance_to_steps(int distance)
+int distance_to_steps(float distance)
 {
-  float revolution = distance / (PI * wheel_diameter);
-  int steps = int(revolution * steps_per_revolution);
+  float revolution; // TODO
+  int steps; // TODO
 
   return steps;
 }
@@ -98,20 +86,12 @@ int distance_to_steps(int distance)
  
 void setup() 
 {
-  Serial.begin(9600);
   
   stepper_l.setSpeed(14);
   stepper_r.setSpeed(14);
 
-  pen_servo.attach(SERVO_PIN);
-
-
 
   // initialise robot
-  pen_up();
-  delay(2000);
-  pen_down();
-
   delay(10000);
   
 }
@@ -119,69 +99,6 @@ void setup()
 
 void loop() 
 {
-
-
-  /*
-  pen_up();
-  delay(2000);
-  pen_down();
-  delay(2000);
-  // draw a rectangle
-  */
-  for (int i = 0; i < 4; i++)
-  {
-    forward(50);
-    turn_right();
-  }
-
-  delay(15000);
-  
-  
-
-
-
-
-
-  /*
-  int length = 0;
-  if (Serial.available())
-  {
-    length = Serial.parseInt();
-    
-    int steps = int(length /3.141592*2/53 * 1024);
-
-    Serial.print("Length: ");
-    Serial.println(length);
-
-
-    //forward(length);
-    
-    if(steps != 0)
-    {
-      stepper_l.setStep(steps);
-      stepper_r.setStep(steps);
- 
-      Serial.print("steps : ");
-      Serial.println(steps); 
-      delay(100);
-    }
-  }
-
-  
-  stepper_l.moveStep();
-  stepper_r.moveStep();
-
-  //Serial.println(stepper_l.MoveOnIs);
-
-
-  
-  
-  //Serial.print("Is Move - stepper : ");
-  //Serial.print(stepper.MoveOnIs);
-  //Serial.print(", stepper2 : ");
-  //Serial.println(stepper2.MoveOnIs);
-
-  */
 
 }
 
